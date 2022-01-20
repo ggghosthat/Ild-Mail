3b45ecf0080fd2d8116a80bb2c766bdf0becc4eb
ggghosthat <ildarildar990@gmail.com>
2022-01-20 22:30:35 +0300

    Functionality - using sending mail proxy for connection to mail server was added

--- src/main/java/com/Ild_Mail/models/Sender.java
+++ src/main/java/com/Ild_Mail/models/Sender.java
@@ -12,6 +12,14 @@ public class Sender {
     private String password = null;
     private InternetAddress toAddress = null;
     private String host = null;
+
+    private Boolean isUsingProxy = false;
+
+    private String proxy_host = null;
+    private String proxy_port = null;
+    private String proxy_user = null;
+    private String proxy_password = null;
+
     private Session session = null;
     private Message message = null;
     private Transport transport = null;
@@ -27,6 +35,20 @@ public class Sender {
         this.host = host;
     }
 
+    public Sender(String from, String password, String to, String host, String proxy_host, int proxy_port, String proxy_user, String proxy_password) throws AddressException {
+        fromAddress = new InternetAddress(from);
+        toAddress = new InternetAddress(to);
+        this.password = password;
+        this.host = host;
+
+        this.proxy_host = proxy_host;
+        this.proxy_port = String.valueOf(proxy_port);
+        this.proxy_user = proxy_user;
+        this.proxy_password = proxy_password;
+
+        this.isUsingProxy = true;
+    }
+
     private void GenerateSession(){
         Properties properties = System.getProperties();
         properties.put("mail.smtp.ssl.enable", "true");
@@ -35,6 +57,16 @@ public class Sender {
         properties.put("mail.smtp.password", password);
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
+
+        if(isUsingProxy){
+            properties.setProperty("mail.smtp.proxy.host",this.proxy_host);
+            properties.setProperty("mail.smtp.proxy.port",this.proxy_port);
+
+            if(this.proxy_user !=  null && this.proxy_password != null && this.proxy_user !=  "" && this.proxy_password != "") {
+                properties.setProperty("mail.smtp.proxy.user", this.proxy_user);
+                properties.setProperty("mail.smtp.proxy.password", this.proxy_password);
+            }
+        }
 
         session = Session.getDefaultInstance(properties);
         session.setDebug(true);
